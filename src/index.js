467019dce7a4e4b11db0e38d7d51cb518ed1b966
Senavarrete2775 <se.navarretep@duocuc.cl>
2025-10-21 01:14:39 -0300

    menu principal casi listoco

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/Style.css'; // Asegúrate que esta ruta es la correcta
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
