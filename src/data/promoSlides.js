1c2ca18f329a30131a600a380cffb8fefc350b02
Senavarrete2775 <se.navarretep@duocuc.cl>
2025-10-26 21:12:38 -0300

    se agrega imagenes carrusel tienda

--- src/data/promoSlides.js
+++ src/data/promoSlides.js
@@ -3,21 +3,21 @@ export const promoSlides = [
         id: 1,
         title: '¡Descuento Exclusivo!',
         subtitle: 'Usuarios de Duoc UC tienen un 20% OFF siempre.',
-        imageUrl: '/images/slider/PromoCarrusel.png',
+        imageUrl: '/images/slider/PromoCarruselDuoc.png',
         link: '/registro'
     },
     {
         id: 2,
         title: 'Equipa tu Setup',
         subtitle: 'Descubre las mejores Sillas Gamer y Accesorios.',
-        imageUrl: '/images/slider/PromoCarrusel.png',
+        imageUrl: '/images/slider/PromoCarruselSillas.png',
         link: '/tienda'
     },
     {
         id: 3,
         title: '¡Sube de Nivel!',
         subtitle: 'Acumula Puntos LevelUp con cada compra.',
-        imageUrl: '/images/slider/PromoCarrusel.png',
+        imageUrl: '/images/slider/PromoCarruselPuntos.png',
         link: '/nosotros'
     }
 ];
